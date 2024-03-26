d9c0ff5b9ea43ac537abeaa177ab69abfa278ed3
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-03-26 18:18:41 +0200

    error page button change

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -13,14 +13,12 @@ const ErrorPage = () => (
       changed or is temporarily unavailable.
     </p>
 
-    <Link to={"/"}>
-      <div className="flex items-center justify-center">
-        <button
-          type="button"
-          className="mb-2 me-2 rounded-full border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
-          Go to Homepage
-        </button>
-      </div>
+    <Link
+      to={"/"}
+      className={
+        "text-center block ml-auto mr-auto my-10 max-w-40 mb-2 me-2 rounded-full border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      }>
+      <span>Go to Homepage</span>
     </Link>
   </>
 );
